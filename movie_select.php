7cd4ba6072af1cacebd4c5e81146b42222e0e961
shreyask22 <69723018+shreyask22@users.noreply.github.com>
2021-01-09 23:14:56 +0530

    commit after overall changes

--- movie_select.php
+++ movie_select.php
@@ -13,12 +13,12 @@ while($row = mysqli_fetch_array($sql)){
 <link rel="stylesheet" href="css/styles.css">
 <body>
 
-<div class="form_display" id="grad" style="float:left">
+<div class="form_display" id="grad" style="float:left;">
 <form action="" method="POST" id="movie_form">
-<input type="date" name="date_select" style="margin:10px" class="desc_submit">
+<input type="date" name="date_select" style="margin:10px; " class="desc_submit">
 <br>
 <select name="time_select" required>
-<option value="choose time">[Choose Time]</option>
+<option value="choose time">Choose Time</option>
 <option value="10:00:00"><time>10:00AM [10:00]</time></option>
 <option value="13:00:00"><time>01:00PM [13:00]</time></option>
 <option value="16:00:00"><time>04:00PM [16:00]</time></option>
@@ -27,7 +27,7 @@ while($row = mysqli_fetch_array($sql)){
 </select>
 <br>
  <select name="movie_select">
- <option value="No movie is selected">[Choose Movie below]</option>
+ <option value="No movie is selected">Choose Movie below</option>
  <?php
  foreach($names as $name){
    echo '<option value="'.$name.'">'.$name.'</option>';
@@ -36,7 +36,7 @@ while($row = mysqli_fetch_array($sql)){
 </select>
 <br>
 <select name="seat_select" required>
-<option value="No. of seats not selected">[Choose no. of seats]</option>
+<option value="No. of seats not selected">Choose no. of seats</option>
 <option value="1">1</option>
 <option value="2">2</option>
 <option value="3">3</option>
@@ -49,7 +49,7 @@ while($row = mysqli_fetch_array($sql)){
 <option value="10">10</option>
 </select> 
 <br>
-<input type="submit" value="select" class="submit" style="border-radius: 10px;background: rgba(255, 255, 255, 0.2);">
+<input type="submit" value="select" class="submit uni-btn">
 </form>
 </div>
 <br>
@@ -63,13 +63,13 @@ $seat_select = filter_input(INPUT_POST, 'seat_select');
 ?>
 <!-- collecting data from user and keepting them in session variables -->
 <div class="show_display" style="float:right">
-<h4>Selection will be displayed here</h4>
-<h4>Movie: <?php echo $movie_select; $_SESSION["session_movie"] = $movie_select; ?> </h4>
-<h4>Seats: <?php echo $seat_select; $_SESSION["session_seat"] = $seat_select;  ?> </h4>
-<h4>Date: <?php echo $date_select; $_SESSION["session_date"] = $date_select;  ?> </h4>
-<h4>Time: <?php echo $time_select; $_SESSION["session_time"] = $time_select;  ?> </h4>
+<p>Selection will be displayed here</p>
+<p>Movie: <?php echo $movie_select; $_SESSION["session_movie"] = $movie_select; ?> </p>
+<p>Seats: <?php echo $seat_select; $_SESSION["session_seat"] = $seat_select;  ?> </p>
+<p>Date: <?php echo $date_select; $_SESSION["session_date"] = $date_select;  ?> </p>
+<p>Time: <?php echo $time_select; $_SESSION["session_time"] = $time_select;  ?> </p>
 <br>
-<input type="button" value="Book" onclick="book_ticket()" href="movie_selectQ.php" style="border-radius: 10px;background: rgba(255, 255, 255, 0.2);">
+<input type="button" class="uni-btn" value="Book" onclick="book_ticket()" href="movie_selectQ.php" style="margin-left:60px">
 </div>
 <!--checking if all fileds are selected -->
 <?php if(!empty($seat_select) && !empty($movie_select) && !empty($date_select) && !empty($time_select))
