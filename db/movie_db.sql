6c34f3e59dd2366fff7cfda6be00179710798424
shreyask22 <69723018+shreyask22@users.noreply.github.com>
2021-01-18 00:14:00 +0530

    Add files via upload

--- db/movie_db.sql
+++ db/movie_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 16, 2021 at 10:26 PM
+-- Generation Time: Jan 17, 2021 at 07:32 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -29,20 +29,21 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `TAX_CALC` ()  BEGIN
 DECLARE ch_done INT DEFAULT 0;
 DECLARE C_A INT(10);
 DECLARE C_B INT(10);
+DECLARE C_C INT(10);
 DECLARE C_net_amt DEC(10,2);
 
 DECLARE C_tax DEC(10,2);
 
 DECLARE C_Tprice CURSOR FOR
-SELECT `price` AS A,`movie_id` AS B FROM `movie` WHERE `net_amt` IS NULL 
+SELECT `price` AS A,`movie_id` AS B,`tax` AS C FROM `movie` WHERE `net_amt` IS NULL 
 FOR UPDATE;
 
 DECLARE CONTINUE HANDLER FOR NOT FOUND SET ch_done = 1;
 
 OPEN C_Tprice;
-FETCH C_Tprice INTO C_A,C_B;
+FETCH C_Tprice INTO C_A,C_B,C_C;
 
-SET C_tax = 0.08;
+SET C_tax = (C_C/100);
 
 SET C_net_amt=C_A + (C_A*C_tax);
 UPDATE `movie` SET `net_amt`=C_net_amt where `movie_id`=C_B;
@@ -103,7 +104,6 @@ INSERT INTO `movie` (`movie_id`, `movie_name`, `movie_lang`, `movie_genre`, `rat
 (19, 'Doctor Strange', 'English', 'Action', 7, 'Doctor Strange is a 2016 American superhero film based on the Marvel Comics character of the same name. Produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures, it is the 14th film in the Marvel Cinematic Universe', 350, 8, 378),
 (20, 'Hobbit', 'English', 'Adventure', 10, 'The Hobbit is a film series consisting of three high fantasy adventure films directed by Peter Jackson. The three films are The Hobbit: An Unexpected Journey, The Hobbit: The Desolation of Smaug, and The Hobbit: The Battle of the Five Armies', 410, 8, 443),
 (21, 'Godzilla', 'English', 'Action', 9, 'The members of Monarch, an crypto-zoological organisation, must rely on the Godzilla and Mothra to defeat King Ghidorah and Rodan, after the former awakens other dormant Titans to destroy the world.', 460, 8, 497),
-(22, 'Lord of the Rings', 'English', 'Adventure', 8, 'A young hobbit, Frodo, who has found the One Ring that belongs to the Dark Lord Sauron, begins his journey with eight companions to Mount Doom, the only place where it can be destroyed.', 370, 8, 400),
 (23, 'Wall E', 'English', 'Adventure', 9, 'A machine responsible for cleaning a waste-covered Earth meets another robot and falls in love with her. Together, they set out on a journey that will alter the fate of mankind.', 380, 8, 410);
 
 -- --------------------------------------------------------
@@ -126,7 +126,6 @@ INSERT INTO `poster` (`movie_name`, `poster_img`) VALUES
 ('Doctor Strange', 'strange.jpg'),
 ('Hobbit', 'hobbit.jpg'),
 ('Godzilla', 'godzilla.jpg'),
-('Lord of the Rings', 'lotr.jpg'),
 ('Wall E', 'walle.jpg');
 
 -- --------------------------------------------------------
@@ -166,7 +165,16 @@ INSERT INTO `seats` (`seat_id`, `date_time`, `movie_id`, `user_id`, `no_seat`) V
 (16, '2021-01-14 16:00:00', 1, 29, 2),
 (17, '2021-01-19 13:00:00', 2, 29, 1),
 (18, '2021-01-14 13:00:00', 1, 29, 1),
-(19, '2021-01-24 10:00:00', 9, 34, 2);
+(19, '2021-01-24 10:00:00', 9, 34, 2),
+(20, '2021-01-17 10:00:00', 20, 29, 2),
+(21, '2021-01-24 22:00:00', 19, 29, 2),
+(22, '2021-01-29 16:00:00', 18, 30, 1),
+(23, '2021-01-23 10:00:00', 23, 29, 2),
+(24, '2021-01-28 10:00:00', 21, 29, 1),
+(25, '2021-01-27 16:00:00', 23, 30, 3),
+(26, '2021-01-16 10:00:00', 23, 30, 3),
+(27, '2021-01-23 10:00:00', 19, 34, 4),
+(28, '2021-01-08 13:00:00', 18, 30, 3);
 
 -- --------------------------------------------------------
 
@@ -176,34 +184,51 @@ INSERT INTO `seats` (`seat_id`, `date_time`, `movie_id`, `user_id`, `no_seat`) V
 
 CREATE TABLE `snacks` (
   `seat_id` int(11) NOT NULL,
-  `popcorn` int(5) NOT NULL,
-  `drink` int(5) NOT NULL
+  `snack_name` varchar(20) NOT NULL,
+  `snack_no` int(5) NOT NULL,
+  `snack_amt` int(11) NOT NULL DEFAULT 50
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `snacks`
 --
 
-INSERT INTO `snacks` (`seat_id`, `popcorn`, `drink`) VALUES
-(1, 1, 2),
-(2, 1, 0),
-(1, 1, 2),
-(1, 1, 2),
-(1, 1, 2),
-(6, 3, 1),
-(7, 2, 1),
-(8, 3, 0),
-(9, 1, 1),
-(10, 1, 0),
-(11, 1, 0),
-(12, 1, 1),
-(13, 1, 2),
-(14, 1, 2),
-(15, 2, 1),
-(16, 1, 0),
-(17, 1, 2),
-(18, 0, 0),
-(19, 0, 1);
+INSERT INTO `snacks` (`seat_id`, `snack_name`, `snack_no`, `snack_amt`) VALUES
+(1, '1', 2, 0),
+(2, '1', 0, 0),
+(1, '1', 2, 0),
+(1, '1', 2, 0),
+(1, '1', 2, 0),
+(6, '3', 1, 0),
+(7, '2', 1, 0),
+(8, '3', 0, 0),
+(9, '1', 1, 0),
+(10, '1', 0, 0),
+(11, '1', 0, 0),
+(12, '1', 1, 0),
+(13, '1', 2, 0),
+(14, '1', 2, 0),
+(15, '2', 1, 0),
+(16, '1', 0, 0),
+(17, '1', 2, 0),
+(18, '0', 0, 0),
+(19, '0', 1, 0),
+(21, 'popcorn', 1, 100),
+(21, 'drink', 2, 100),
+(22, 'popcorn', 1, 100),
+(22, 'drink', 1, 100),
+(23, 'popcorn', 1, 100),
+(23, 'drink', 1, 100),
+(24, 'popcorn', 1, 100),
+(24, 'drink', 2, 100),
+(25, 'popcorn', 1, 100),
+(25, 'drink', 0, 100),
+(26, 'popcorn', 1, 100),
+(26, 'drink', 1, 100),
+(27, 'popcorn', 3, 50),
+(27, 'drink', 0, 50),
+(28, 'popcorn', 1, 50),
+(28, 'drink', 2, 50);
 
 -- --------------------------------------------------------
 
@@ -325,13 +350,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `movie_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `movie_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `seats`
 --
 ALTER TABLE `seats`
-  MODIFY `seat_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `seat_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `ticket`
